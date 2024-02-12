abe5805c742a4f3971f51b9d7ed56c3861261ddf
vicsnet <adesanmiadebisi@gmail.com>
2024-02-12 16:53:22 +0100

    functions with return value

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
-    let y = 6;
+    let _y = 6;
 
 
     println!("Hello, world!");
     another_function(5);
     print_labeled_measurement(10, 'g');
+
+    let x = five();
+    println!("the value of x is:{x}");
 }
 fn another_function(x: i32){
     println!("Another function");
@@ -15,4 +18,8 @@ fn another_function(x: i32){
 
 fn print_labeled_measurement(value: i32, uint_label: char){
     println!("The measurement is: {value}{uint_label}");
+}
+
+fn five()->i32{
+    5
 }
