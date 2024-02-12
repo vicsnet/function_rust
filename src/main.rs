cabae1b49fb35097c4aed32736b0ef487576c118
vicsnet <adesanmiadebisi@gmail.com>
2024-02-12 16:32:10 +0100

    parameters

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    another_function(5);
+}
+fn another_function(x: i32){
+    println!("Another function");
+
+    println!("the value of x is: {x}");
+}
