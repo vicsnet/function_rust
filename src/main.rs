c1fb71be85a27a1379c867ac359e3e64f6e11b29
vicsnet <adesanmiadebisi@gmail.com>
2024-02-12 16:35:36 +0100

    multiple parameters

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
     println!("Hello, world!");
     another_function(5);
+    print_labeled_measurement(10, 'g');
 }
 fn another_function(x: i32){
     println!("Another function");
 
     println!("the value of x is: {x}");
+
+}
+
+fn print_labeled_measurement(value: i32, uint_label: char){
+    println!("The measurement is: {value}{uint_label}");
 }
